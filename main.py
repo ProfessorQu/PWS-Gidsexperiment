dfe5c7078867ad553c7307bac86860b071c15d1f
ProfessorQu <quint@dasookwat.com>
2022-12-06 17:42:07 +0100

    Some refractoring to make it less cluttered

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import List
 import pygame
 from pygame.locals import *
@@ -22,7 +23,7 @@ def main():
 
     spawning1 = False
     spawning2 = False
-    spawning3 = False
+    removing = False
 
     grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
 
@@ -34,26 +35,23 @@ def main():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     spawning1 = True
+                elif event.button == 2:
+                    removing = True
                 elif event.button == 3:
                     spawning2 = True
 
             if event.type == MOUSEBUTTONUP:
                 if event.button == 1:
                     spawning1 = False
+                elif event.button == 2:
+                    removing = False
                 elif event.button == 3:
                     spawning2 = False
-                
-            if event.type == KEYDOWN:
-                if event.key == K_r:
-                    grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
-                elif event.key == K_SPACE:
-                    spawning3 = True
-            
-            if event.type == KEYUP:
-                if event.key == K_SPACE:
-                    spawning3 = False
-            
-            spawn(spawning1, spawning2, spawning3, grid)
+
+            if event.type == KEYDOWN and event.key == K_r:
+                grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+
+            spawn(spawning1, spawning2, removing, grid)
 
         SCREEN.fill((150, 150, 150))
 
@@ -66,30 +64,21 @@ def main():
         CLOCK.tick(1_000_000)
 
 
-def spawn(spawning1: bool, spawning2: bool, spawning3: bool, grid):
-    if spawning1:
-        x = int((pygame.mouse.get_pos()[0] / WIDTH) * GRID_WIDTH)
-        y = int((pygame.mouse.get_pos()[1] / HEIGHT) * GRID_HEIGHT)
-        for cx in range(-3, 4):
-            for cy in range(-3, 4):
-                if in_bounds(x + cx, y + cy):
-                    grid[y + cy][x + cx] = 1
-
+def spawn(spawning1: bool, spawning2: bool, removing: bool, grid):
+    if removing:
+        set_square(-4, 5, 0, grid)
+    elif spawning1:
+        set_square(-3, 4, 1, grid)
     elif spawning2:
-        x = int((pygame.mouse.get_pos()[0] / WIDTH) * GRID_WIDTH)
-        y = int((pygame.mouse.get_pos()[1] / HEIGHT) * GRID_HEIGHT)
-        for cx in range(-1, 2):
-            for cy in range(-1, 2):
-                if in_bounds(x + cx, y + cy):
-                    grid[y + cy][x + cx] = 2
-    
-    elif spawning3:
-        x = int((pygame.mouse.get_pos()[0] / WIDTH) * GRID_WIDTH)
-        y = int((pygame.mouse.get_pos()[1] / HEIGHT) * GRID_HEIGHT)
-        for cx in range(-1, 2):
-            for cy in range(-1, 2):
-                if in_bounds(x + cx, y + cy):
-                    grid[y + cy][x + cx] = 4
+        set_square(-1, 2, 2, grid)
+
+
+def set_square(lower_bound, upper_bound, type, grid):
+    x = int((pygame.mouse.get_pos()[0] / WIDTH) * GRID_WIDTH)
+    y = int((pygame.mouse.get_pos()[1] / HEIGHT) * GRID_HEIGHT)
+    for cx, cy in itertools.product(range(lower_bound, upper_bound), range(lower_bound, upper_bound)):
+        if in_bounds(x + cx, y + cy):
+            grid[y + cy][x + cx] = type
 
 
 def fps_counter():
@@ -116,89 +105,170 @@ def draw(grid: List[List[int]]):
 
 
 def update(grid: List[List[int]]) -> List[List[int]]:
-    for y in range(GRID_HEIGHT - 1, -1, -1):
-        for x in range(GRID_HEIGHT):
-            # ----- WATER -----
-            if grid[y][x] == 1:
-                if in_bounds(x, y + 1) and grid[y + 1][x] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x] = 1
-                elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x + 1] = 1
-                elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x - 1] = 1
+    for y, x in itertools.product(range(GRID_HEIGHT - 1, -1, -1), range(GRID_HEIGHT)):
+        # ----- WATER -----
+        if grid[y][x] == 1:
+            update_water(grid, x, y)
 
-                else:
-                    rand_dir = 1 if random.random() > 0.5 else -1
-                    if in_bounds(x + rand_dir, y) and grid[y][x + rand_dir] == 0:
-                        grid[y][x] = 0
-                        grid[y][x + rand_dir] = 1
-                    elif in_bounds(x - rand_dir, y) and grid[y][x - rand_dir] == 0:
-                        grid[y][x] = 0
-                        grid[y][x - rand_dir] = 1
+        # ----- SAND -----
+        elif grid[y][x] == 2:
+            update_sand(grid, x, y)
+
+        # ----- PRODUCT -----
+        elif grid[y][x] == 3:
+            update_product(grid, x, y)
+
+
+def update_water(grid: List[List[int]], x: int, y: int):
+    updated = water_fall(grid, x, y)
+
+    if not updated:
+        water_spread(grid, x, y)
+
+def water_fall(grid: List[List[int]], x: int, y: int) -> bool:
+    if in_bounds(x, y + 1) and grid[y + 1][x] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x] = 1
+
+        return True
+    elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x + 1] = 1
+
+        return True
+    elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x - 1] = 1
+
+        return True
+
+def water_spread(grid: List[List[int]], x: int, y: int) -> bool:
+    rand_dir = 1 if random.random() > 0.5 else -1
+    if in_bounds(x + rand_dir, y) and grid[y][x + rand_dir] == 0:
+        grid[y][x] = 0
+        grid[y][x + rand_dir] = 1
+
+        return True
+    elif in_bounds(x - rand_dir, y) and grid[y][x - rand_dir] == 0:
+        grid[y][x] = 0
+        grid[y][x - rand_dir] = 1
             
-            # ----- SAND -----
-            elif grid[y][x] == 2:
-                if in_bounds(x, y + 1) and grid[y + 1][x] == 1:
-                    grid[y][x] = 3
-                    grid[y + 1][x] = 0
-                if in_bounds(x, y - 1) and grid[y - 1][x] == 1:
-                    grid[y][x] = 3
-                    grid[y - 1][x] = 0
+        return True
 
-                elif in_bounds(x, y + 1) and grid[y + 1][x] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x] = 2
-                elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x + 1] = 2
-                elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x - 1] = 2
+def update_sand(grid: List[List[int]], x: int, y: int):
+    updated = sand_react(grid, x, y)
 
-                elif in_bounds(x, y + 1) and grid[y + 1][x] == 1:
-                    grid[y][x] = 1
-                    grid[y + 1][x] = 2
-                elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 1:
-                    grid[y][x] = 1
-                    grid[y + 1][x + 1] = 2
-                elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 1:
-                    grid[y][x] = 1
-                    grid[y + 1][x - 1] = 2
-            
-            # ----- VOMIT -----
-            elif grid[y][x] == 3:
-                if in_bounds(x, y + 1) and grid[y + 1][x] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x] = 3
-                elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x + 1] = 3
-                elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 0:
-                    grid[y][x] = 0
-                    grid[y + 1][x - 1] = 3
+    if not updated:
+        updated = sand_fall(grid, x, y)
+    
+    if not updated:
+        sand_fall_in_water(grid, x, y)
 
-                elif in_bounds(x, y + 1) and grid[y + 1][x] == 1:
-                    grid[y][x] = 1
-                    grid[y + 1][x] = 3
-                elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 1:
-                    grid[y][x] = 1
-                    grid[y + 1][x + 1] = 3
-                elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 1:
-                    grid[y][x] = 1
-                    grid[y + 1][x - 1] = 3
-                    
-                elif in_bounds(x, y + 1) and grid[y + 1][x] == 2:
-                    grid[y][x] = 2
-                    grid[y + 1][x] = 3
-                elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 2:
-                    grid[y][x] = 2
-                    grid[y + 1][x + 1] = 3
-                elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 2:
-                    grid[y][x] = 2
-                    grid[y + 1][x - 1] = 3
+def sand_react(grid: List[List[int]], x: int, y: int) -> bool:
+    if in_bounds(x, y + 1) and grid[y + 1][x] == 1:
+        grid[y][x] = 3
+        grid[y + 1][x] = 0
+
+        return True
+    if in_bounds(x, y - 1) and grid[y - 1][x] == 1:
+        grid[y][x] = 3
+        grid[y - 1][x] = 0
+        
+        return True
+
+def sand_fall(grid: List[List[int]], x: int, y: int) -> bool:
+    if in_bounds(x, y + 1) and grid[y + 1][x] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x] = 2
+
+        return True
+    elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x + 1] = 2
+        
+        return True
+    elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x - 1] = 2
+        
+        return True
+
+def sand_fall_in_water(grid: List[List[int]], x: int, y: int) -> bool:
+    if in_bounds(x, y + 1) and grid[y + 1][x] == 1:
+        grid[y][x] = 1
+        grid[y + 1][x] = 2
+        
+        return True
+    elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 1:
+        grid[y][x] = 1
+        grid[y + 1][x + 1] = 2
+        
+        return True
+    elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 1:
+        grid[y][x] = 1
+        grid[y + 1][x - 1] = 2
+        
+        return True
+
+def update_product(grid: List[List[int]], x: int, y: int):
+    updated = product_fall(grid, x, y)
+
+    if not updated:
+        updated = product_fall_in_water(grid, x, y)
+    
+    if not updated:
+        product_fall_in_sand(grid, x, y)
+
+def product_fall(grid: List[List[int]], x: int, y: int) -> bool:
+    if in_bounds(x, y + 1) and grid[y + 1][x] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x] = 3
+
+        return True
+    elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x + 1] = 3
+
+        return True
+    elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 0:
+        grid[y][x] = 0
+        grid[y + 1][x - 1] = 3
+
+        return True
+
+def product_fall_in_water(grid: List[List[int]], x: int, y: int) -> bool:
+    if in_bounds(x, y + 1) and grid[y + 1][x] == 1:
+        grid[y][x] = 1
+        grid[y + 1][x] = 3
+
+        return True
+    elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 1:
+        grid[y][x] = 1
+        grid[y + 1][x + 1] = 3
+
+        return True
+    elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 1:
+        grid[y][x] = 1
+        grid[y + 1][x - 1] = 3
+
+        return True
+
+def product_fall_in_sand(grid: List[List[int]], x: int, y: int) -> bool:
+    if in_bounds(x, y + 1) and grid[y + 1][x] == 2:
+        grid[y][x] = 2
+        grid[y + 1][x] = 3
+
+        return True
+    elif in_bounds(x + 1, y + 1) and grid[y + 1][x + 1] == 2:
+        grid[y][x] = 2
+        grid[y + 1][x + 1] = 3
+
+        return True
+    elif in_bounds(x - 1, y + 1) and grid[y + 1][x - 1] == 2:
+        grid[y][x] = 2
+        grid[y + 1][x - 1] = 3
+
+        return True
 
 
 if __name__ == "__main__":
